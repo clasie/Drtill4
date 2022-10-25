16b587c4c2393bbee8d45803f9f3baa78f23e99c
clasie <claude_siefers@hotmail.com>
2022-10-25 09:35:42 +0200

    koff Drill 4

---
+++ CoreWinFormsApp4/LoginUI.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CoreWinFormsApp4
+{
+    partial class LoginUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_validate = new System.Windows.Forms.Button();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_validate
+            // 
+            this.btn_validate.Location = new System.Drawing.Point(283, 277);
+            this.btn_validate.Name = "btn_validate";
+            this.btn_validate.Size = new System.Drawing.Size(94, 29);
+            this.btn_validate.TabIndex = 0;
+            this.btn_validate.Text = "Validate";
+            this.btn_validate.UseVisualStyleBackColor = true;
+            this.btn_validate.Click += new System.EventHandler(this.btn_validate_Click);
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(113, 85);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(49, 20);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "Name";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(113, 178);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(70, 20);
+            this.lbl_password.TabIndex = 2;
+            this.lbl_password.Text = "Password";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(113, 118);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(433, 27);
+            this.txt_name.TabIndex = 3;
+            this.txt_name.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_name_KeyDown);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(113, 216);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(433, 27);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_password_KeyDown);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(422, 277);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(94, 29);
+            this.btn_cancel.TabIndex = 5;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // LoginUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.btn_validate);
+            this.Name = "LoginUI";
+            this.Text = "LoginUI";
+            this.Load += new System.EventHandler(this.LoginUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_validate;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
---
+++ CoreWinFormsApp4/LoginUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreWinFormsApp4
+{
+    public partial class LoginUI : Form
+    {
+        public bool IsLogged = false;
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_validate_Click(object sender, EventArgs e)
+        {
+            if (txt_name.Text == "" || txt_password.Text == "")
+            {
+                MessageBox.Show("Empty creds");
+                return;
+            }
+            else if (txt_name.Text == "a" || txt_password.Text == "a")
+            {
+                this.IsLogged = true;
+                this.Close();
+            }
+            else {
+                MessageBox.Show("Bad creds");
+            }
+        }
+
+        private void txt_name_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_validate.PerformClick();
+            }
+        }
+
+        private void txt_password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_validate.PerformClick();
+            }
+        }
+    }
+}
